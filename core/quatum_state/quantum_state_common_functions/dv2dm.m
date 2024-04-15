d44bd1a2c63464dd2dcb0f419bc759e0c9004f82
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:19:40 +0200

    Update dv2dm.m

--- core/quatum_state/quantum_state_common_functions/dv2dm.m
+++ core/quatum_state/quantum_state_common_functions/dv2dm.m
@@ -1,9 +1,10 @@
 function [dm] = dv2dm(dv)
-    %DV2DM: Density Vector to Density matrix. From a |rho) which is (n*n) x 1
+    %DV2DM: Density Vector to Density matrix. From a |rho) which is (n * n) x 1
     %dimensional vector we get a n x n dimensional matrix
-    NoQ = log(sqrt(length(dv)))/log(2);
-    dv = InverseBasisTransform(dv, 'v', NoQ );
 
-    dm = reshape(dv, [2^NoQ 2^NoQ]).';
+    NoQ = log(sqrt(length(dv)))/log(2);
+    dv  = InverseBasisTransform(dv, 'v', NoQ );
+
+    dm  = reshape(dv, [2^NoQ 2^NoQ]).';
 end
 
