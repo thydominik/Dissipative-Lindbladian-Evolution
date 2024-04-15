5a022160459bba41526e0901ab909e2124712ce4
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 16:07:47 +0200

    Update MatrixAntiCommutator.m

--- core/common_functions/MatrixAntiCommutator.m
+++ core/common_functions/MatrixAntiCommutator.m
@@ -1,14 +1,13 @@
 function [C] = MatrixAntiCommutator(A, B)
-    %MATRIXANTICOMMUTATOR takes two matricies and computes the anticommutator:
-    %{A, B} = A*B + B*A
-    
-    size_a = size(A);
-    size_b = size(B);
-    
-    if size_a ~= size_b
-        warning("Matrix dimension mismatch")
-    else
-        C = A * B + B * A;
-    end
-end
+% MATRIXANTICOMMUTATOR takes two matricies and computes the anticommutator:
+%{A,B} = A*B + B*A
 
+size_a = size(A);
+size_b = size(B);
+
+if size_a ~= size_b
+    warning("Matrix dimension mismatch")
+else
+    C = A * B + B * A;
+end
+end
