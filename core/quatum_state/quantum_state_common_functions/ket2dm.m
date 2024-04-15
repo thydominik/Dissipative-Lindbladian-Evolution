945d92e0efebbccc3658363173e7852c9a668251
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:21:39 +0200

    Update ket2dm.m

--- core/quatum_state/quantum_state_common_functions/ket2dm.m
+++ core/quatum_state/quantum_state_common_functions/ket2dm.m
@@ -5,13 +5,6 @@ if length(ket) >= 3
     warning('This is not a 2 state state')
 end
 
-%Make sure that this is a cloumn vector
-% psi = zeros(length(ket), 1);
-% 
-% for i = 1:length(ket)
-%     psi(i) = ket(i);
-% end
-
 if size(ket, 1) == 1
     psi = ket.';
 else
@@ -19,6 +12,7 @@ else
 end
 
 rho = psi * psi';
+
 end
 
 
