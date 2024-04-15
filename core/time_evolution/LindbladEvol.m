56562f6f54dbd087b325a32f0cc1e0b8e54f246f
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:42:11 +0200

    Update LindbladEvol.m

--- core/time_evolution/LindbladEvol.m
+++ core/time_evolution/LindbladEvol.m
@@ -1,22 +1,23 @@
-function [LL] = LindbladEvol(Hami, F, G)
+function [LL] = LindbladEvol(Hamiltonian, F_op, Gamma)
     %LINDBLADEVOL Formulates the Lindbladian operator that acts on the density
     %vector.
-    %Hami - Hamiltonian
-    %F - List of the dissipators
-    %G - List of the dissipator weights
     
-    %Formulateing the Hamiltonian part:
-    L_H = kron(Hami, eye(size(Hami))) - kron(eye(size(Hami)), conj(Hami));
+    % Hami - Hamiltonian
+    % F - List of the dissipators/ jump operators
+    % G - List of the dissipator weights
     
-    %Dissipator part:
+    % Formulateing the Hamiltonian part:
+    L_H = kron(Hamiltonian, eye(size(Hamiltonian))) - kron(eye(size(Hamiltonian)), conj(Hamiltonian));
+    
+    % Dissipator part:
     L_D = zeros(size(L_H));
-    for ind = 1:length(G)
-        n = length(F(ind, :, :));
-        tempF = reshape(F(ind, :, :), [n, n]);
+    for ind = 1:length(Gamma)
+        n = length(F_op(ind, :, :));
+        tempF = reshape(F_op(ind, :, :), [n, n]);
         tempFpF = ctranspose(tempF) * tempF;
         tempMTX = 2 * kron(tempF, conj(tempF)) - kron(tempFpF, eye(size(tempFpF))) - kron(eye(size(tempFpF)), tempFpF);
         
-        L_D = L_D + 1i * 0.5 * G(ind) *  tempMTX;
+        L_D = L_D + 1i * 0.5 * Gamma(ind) *  tempMTX;
     end
     LL = L_H + L_D;
 end
