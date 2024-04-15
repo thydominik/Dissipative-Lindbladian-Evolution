19f9fafcdf9c987f26338656fb859d6ca663767b
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:19:15 +0200

    Update dm2dv.m

--- core/quatum_state/quantum_state_common_functions/dm2dv.m
+++ core/quatum_state/quantum_state_common_functions/dm2dv.m
@@ -1,23 +1,43 @@
 function [RhoVec] = dm2dv(rho)
-    %DM2DV: Density Matrix to Density Vector
-    
-    % This part will order the density matrix into the density vector in
-    % way that preserves the order of the mtx rows. I prefer this way,
-    % because then we don't have to fiddle with the operators structure
+%DM2DV: Density Matrix to Density Vector
+
+% This part will order the density matrix into the density vector in a
+% way that preserves the order of the mtx rows. This is the preferred way due to the operator constrcution
+
+DensityMtxSize = size(rho);
+
+NumberOfQubits = log(DensityMtxSize(1))/log(2);
+
+noq     = NumberOfQubits;
+sizev   = [2 4 8 16 32 64 128 256 512 1024 2048];
+sizes   = 2^noq;
+
+NewX = rho;
+for i = 1:(noq - 1)
+    C = mat2cell(NewX, [ones(1, 2^(2*i-1)) * sizev(noq - i)], [sizes/(2^i) sizes/(2^i)]).';             %[ones(1, sizesv(i)) * sizes/(2-i)], [ones(1, sizesv(i)) * sizes/(2-i)]).'
+    NewX = [];
+    for j = 1:2^(2 * i)
+        NewX = [NewX; C{j}];
+    end
+end
+
+NewX    = NewX.';
+RhoVec  = NewX(:);
+end
+
+
 %     Size = size(rho);
-%     
+%
 % %     for i = 1:Size(1)
 % %         for j = 1:Size(2)
-% %             dv(Size(2) * (i - 1) + j) = rho(i, j);    
+% %             dv(Size(2) * (i - 1) + j) = rho(i, j);
 % %         end
 % %     end
-% 
+%
 %     dv = reshape(rho.', [Size(1)^2 1]);
-% 
-        DensityMtxSize = size(rho);
-% 
-        NumberOfQubits = log(DensityMtxSize(1))/log(2);
-% 
+%
+
+%
 %     RhoVec = [];
 %     for i = 1:2:DensityMtxSize(1)
 %         for j = 1:2:DensityMtxSize(1)
@@ -26,21 +46,5 @@ function [RhoVec] = dm2dv(rho)
 %             RhoVec      = [RhoVec; tempRhoVec];
 %         end
 %     end
-    
-    % Old -----------------------------------------------------------------
-    noq = NumberOfQubits;
-    sizev = [2 4 8 16 32 64 128 256 512 1024 2048];
-    sizes   = 2^noq;
 
-    NewX = rho;
-    for i = 1:(noq-1)
-        C = mat2cell(NewX, [ones(1, 2^(2*i-1)) * sizev(noq-i)], [sizes/(2^i) sizes/(2^i)]).';%[ones(1, sizesv(i)) * sizes/(2-i)], [ones(1, sizesv(i)) * sizes/(2-i)]).'
-        NewX = [];
-        for j = 1:2^(2*i)
-            NewX = [NewX; C{j}];
-        end
-    end
-    
-    NewX = NewX.';
-    RhoVec = NewX(:);
-end
+% Old -----------------------------------------------------------------
