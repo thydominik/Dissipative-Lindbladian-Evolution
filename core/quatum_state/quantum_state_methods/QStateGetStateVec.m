97166d8b9961f144698ef549bedfb84261f93a71
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:37:17 +0200

    Update QStateGetStateVec.m

--- core/quatum_state/quantum_state_methods/QStateGetStateVec.m
+++ core/quatum_state/quantum_state_methods/QStateGetStateVec.m
@@ -1,19 +1,25 @@
 function [CCoeffs, RelPhase, Psi] = QStateGetStateVec(qstate)
-%QSTATEGETSTATEVEC
-    DensityMatrix = dv2dm(qstate.DensityVector);
+%QSTATEGETSTATEVEC gets the state vector froma  density matrix.
 
-    CCoeffs = sqrt(diag(DensityMatrix));
-    
-    NonZeroElement = find(CCoeffs, 1, 'first');
-    RelPhase(NonZeroElement) = 0;
-    for i = 1:(2^qstate.NumberOfQubits)
-        if CCoeffs(i) ~= 0
-            RelPhase(i) = (-1i) * log(DensityMatrix(NonZeroElement, i) / (CCoeffs(NonZeroElement) * CCoeffs(i)));
-        else
-            RelPhase(i) = 0;
-        end
+% Note: Phase degree of freedom. Not usefull in the case of mixed states.
+
+DensityMatrix = dv2dm(qstate.DensityVector);
+
+CCoeffs = sqrt(diag(DensityMatrix));
+
+NonZeroElement = find(CCoeffs, 1, 'first');
+
+RelPhase(NonZeroElement) = 0;
+
+for i = 1:(2^qstate.NumberOfQubits)
+    if CCoeffs(i) ~= 0
+        RelPhase(i) = (-1i) * log(DensityMatrix(NonZeroElement, i) / (CCoeffs(NonZeroElement) * CCoeffs(i)));
+    else
+        RelPhase(i) = 0;
     end
+end
 
-    Psi = CCoeffs.' .* exp(1i .* RelPhase);
+Psi = CCoeffs.' .* exp(1i .* RelPhase);
+
 end
 
