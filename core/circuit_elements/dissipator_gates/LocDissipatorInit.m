bef2d2d686d51fbdeeff736aaa91ed1fb7f26ac6
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 11:53:08 +0200

    Format, comment, new functionality

--- core/circuit_elements/dissipator_gates/LocDissipatorInit.m
+++ core/circuit_elements/dissipator_gates/LocDissipatorInit.m
@@ -1,20 +1,28 @@
 function [Dissipator] = LocDissipatorInit(DissipatorInput)
-    %LOCDISSIPATORINIT
-    % Input
-    % OutPut
-    % Mit csinál?
-    % C - Lapack mint példa
-    Dissipator = struct();
-    
-    [Row, Col] = size(DissipatorInput);
-    if Row ~= Col
-        error('Not square matrix')
-    else 
-        Dissipator.Info = '1 qubit Local Dissipator';
-        Dissipator.Type = 'Dissipator Gate';
-        Dissipator.NumberOfQubits = 1;
-        Dissipator.Operator = @(g) sqrt(g) * sparse(DissipatorInput);
-        %Dissipator.ActingOnQubit = ActOnQubit;
-    end
+%LOCDISSIPATORINIT - Creates a local dissipator for N qubits. This N is defined by the size of
+%the DissipatorIntup input.
+
+% INPUT --------------------------------------------------
+% DissipatorInput - [Matrix], A square matrix of size 2x2, 4x4, 8x8, ... that describes a dissipator
+% on 1, 2, 3, ... qubits.
+% INPUT --------------------------------------------------
+
+Dissipator = struct();
+
+[Row, Col]  = size(DissipatorInput);
+N           = log2(Row);
+if Row ~= Col
+    error('Not square matrix')
+end
+
+if mod(N, 2) ~= 0
+    error('Wrong Dissipator Input');
+end
+
+    Dissipator.Info             = num2str(N) + " qubit Local Dissipator";
+    Dissipator.Type             = "Dissipator Gate";
+    Dissipator.NumberOfQubits   = N;
+    Dissipator.Operator         = @(g) sqrt(g) * sparse(DissipatorInput);
+
 end
 
