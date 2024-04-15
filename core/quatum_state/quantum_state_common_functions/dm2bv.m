4cfc216b315ab14ddb198ba9235bc4e811084b4b
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:17:08 +0200

    Update dm2bv.m

--- core/quatum_state/quantum_state_common_functions/dm2bv.m
+++ core/quatum_state/quantum_state_common_functions/dm2bv.m
@@ -5,8 +5,7 @@ Sx = [0 1; 1 0];
 Sz = [1 0; 0 -1];
 Sy = 1i * Sx * Sz;
 
-BV = [trace(Sx * rho); trace(Sy * rho); trace(Sz * rho)];
-%BV = [(2 * real(rho(1,2))); 2*imag(rho(2,1)); rho(1,1) - rho(2,2)]
-Norm = norm(BV);
+BV      = [trace(Sx * rho); trace(Sy * rho); trace(Sz * rho)];  
+Norm    = norm(BV);                                             % important for dissipative systems.
 end
 
