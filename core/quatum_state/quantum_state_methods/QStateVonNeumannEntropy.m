3c315b33eb14c486b4937bf56bcc30aed965b767
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:39:47 +0200

    Update QStateVonNeumannEntropy.m

--- core/quatum_state/quantum_state_methods/QStateVonNeumannEntropy.m
+++ core/quatum_state/quantum_state_methods/QStateVonNeumannEntropy.m
@@ -1,12 +1,15 @@
 function [S] = QStateVonNeumannEntropy(QSystem)
-    DensityMatrix = dv2dm(QSystem.DensityVector);
-    Lambda = eig(DensityMatrix);
+% Calculates the neumann entrpy for a given quantum system:
+    DensityMatrix   = dv2dm(QSystem.DensityVector);
+    Lambda          = eig(DensityMatrix);
+
     S = 0;
+
     for i = 1:length(Lambda)
         if Lambda(i) == 0
             S = S;
         else
-            S = S + Lambda(i)*log(1/Lambda(i));
+            S = S + Lambda(i) * log(1/Lambda(i));
         end
     end
     QSystem.Entropy = real(S);
