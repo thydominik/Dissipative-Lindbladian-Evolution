c904bc289e0e3c4163f65558d20fff57a449f23c
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:22:07 +0200

    Update NQubitBasis.m

--- core/quatum_state/quantum_state_common_functions/NQubitBasis.m
+++ core/quatum_state/quantum_state_common_functions/NQubitBasis.m
@@ -1,6 +1,6 @@
 function [Basis] = NQubitBasis(N)
     %NQUBITBASIS: Creates (just for show) an N - qubit basis
-
+% OLD --------------------------------------------
 % Basis = zeros(2^N, floor(1 + log(N)/(log(2))));
 % S = floor(1 + log(N)/(log(2)));
 % for i = 1:2^N
@@ -10,9 +10,9 @@ function [Basis] = NQubitBasis(N)
 %     b = [temp_zeros temp]
 %     Basis(i, :) = b;
 % end
+% OLD --------------------------------------------
 
 Basis = dec2bin(0:1:2^N-1)-'0';
-
 
 end
 
