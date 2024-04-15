a96abe95d5a83ca90de67165c439906122802926
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 15:37:02 +0200

    Update SpinOp.m

--- core/circuit_elements/operators/SpinOp.m
+++ core/circuit_elements/operators/SpinOp.m
@@ -1,20 +1,20 @@
 function [S] = SpinOp(handle)
-    %SPINOP This function creates the spin - 1/2 spin operators from the
-    %command SpinOp(handle); The 'handle' can be 1 - x, 2 - y, 3 - z, 4 -
-    %p, 5 - m
+% SPINOP This function creates the spin - 1/2 spin operators from the
+% command SpinOp(handle); The 'handle' can be 1 - x, 2 - y, 3 - z, 4 -
+% p, 5 - m
 
-    if handle == 1 || handle == 'x'
-        S = Pauli('x');
-    elseif handle == 2 || handle == 'y'
-        S = Pauli('y');
-    elseif handle == 3 || handle == 'z'
-        S = Pauli('z');
-    elseif handle == 4 || handle == 'p'
-        S = [0 1; 0 0];
-    elseif handle == 5 || handle == 'm'
-        S = [0 0; 0 1];
-    else
-        warning('Input not appropriate, try {(1, 2, 3, 4, 5) or {x, y, z, p, m}}')
-    end
+if handle == 1 || handle == 'x'
+    S = Pauli('x');
+elseif handle == 2 || handle == 'y'
+    S = Pauli('y');
+elseif handle == 3 || handle == 'z'
+    S = Pauli('z');
+elseif handle == 4 || handle == 'p'
+    S = [0 1; 0 0];
+elseif handle == 5 || handle == 'm'
+    S = [0 0; 0 1];
+else
+    warning('Input not appropriate, try {(1, 2, 3, 4, 5) or {x, y, z, p, m}}')
+end
 end
 
