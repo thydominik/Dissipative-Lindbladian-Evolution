3b26812dfdd972c668d852390c6048ada300f9f0
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:41:15 +0200

    Update dmUnitaryEvol.m

--- core/time_evolution/dmUnitaryEvol.m
+++ core/time_evolution/dmUnitaryEvol.m
@@ -2,6 +2,8 @@ function [new_rho] = dmUnitaryEvol(rho, U)
     %DMUNITARYEVOL Takes a density matrix, and a unitary operator, then
     %"evolves" the density mtx as rho' = U * rho * U^+
     
+    Rounding = 10;
+
     rho_size = size(rho);
     unitary_size = size(U);
     
@@ -9,8 +11,8 @@ function [new_rho] = dmUnitaryEvol(rho, U)
         warning("Incosistent matrix dimensions!")
     else
         new_rho = U * rho * ctranspose(U);
-        if round(trace(new_rho), 5) ~= 1
-            warning("Operator not unitary!")
+        if round(trace(new_rho), Rounding) ~= 1
+            warning("Operator not unitary! Check rounding and/or operator!")
         end
     end
 
