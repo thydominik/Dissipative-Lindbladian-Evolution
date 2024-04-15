6b392559b7e1e49e7c52c863d516053b74bd5253
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 16:08:14 +0200

    Update MatrixCommutator.m

--- core/common_functions/MatrixCommutator.m
+++ core/common_functions/MatrixCommutator.m
@@ -1,15 +1,13 @@
 function [C] = MatrixCommutator(A, B)
-    %MATRIXCOMMUTATOR: Takes two matricies and produces their commutator:
-    % [A, B] = A*B - B*A
-    
-    size_a = size(A);
-    size_b = size(B);
-    
-    if size_a ~= size_b 
-        warning("Matrix dimension mismatch")
-    else
-        C = A * B - B * A;
-    end
+%MATRIXCOMMUTATOR: Takes two matricies and produces their commutator:
+%[A,B] = A*B - B*A
 
+size_a = size(A);
+size_b = size(B);
+
+if size_a ~= size_b
+    warning("Matrix dimension mismatch")
+else
+    C = A * B - B * A;
 end
-
+end
