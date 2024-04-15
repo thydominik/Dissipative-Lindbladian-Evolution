c6f752741f8d6318f02ba39c11b77e7742f30155
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 12:14:30 +0200

    Update Fidelity.m

--- core/circuit_elements/measurement/Fidelity.m
+++ core/circuit_elements/measurement/Fidelity.m
@@ -1,14 +1,21 @@
 function [F] = Fidelity(QSystemA, QSystemB)
-    if QSystemA.NumberOfQubits == QSystemB.NumberOfQubits
+% FIDELITY - Measures the fidelity of system A to system B
 
-        DensityMatrix_A = dv2dm(QSystemA.DensityVector);
-        Psi_A = QStateGetStateVec(QSystemA);
-        DensityMatrix_B = dv2dm(QSystemB.DensityVector);
-        Psi_B = QStateGetStateVec(QSystemB);
-        F = (trace((DensityMatrix_A * DensityMatrix_B)));
-    else
-        warning('System size mismatch; Fidelity is 0')
-        F = 0;
-    end
+% INPUT --------------------------------------------------
+% QSystem A - [Quantum system], A quantum system that will be compared to QSystem B. This Quantum
+% system variable is sued by this ED code.
+% INPUT --------------------------------------------------
+
+if QSystemA.NumberOfQubits == QSystemB.NumberOfQubits
+
+    % getting the density matrix from the density vectors for both systems
+    DensityMatrix_A = dv2dm(QSystemA.DensityVector);
+    DensityMatrix_B = dv2dm(QSystemB.DensityVector);
+
+    F = (trace(DensityMatrix_A * DensityMatrix_B));
+else
+    warning('System size mismatch; Fidelity is 0')
+    F = 0;
+end
 end
 
