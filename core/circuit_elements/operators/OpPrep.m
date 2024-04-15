c1e52850e3d2addf2e428d206328f52c4ce66ec1
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 15:39:00 +0200

    Update OpPrep.m

--- core/circuit_elements/operators/OpPrep.m
+++ core/circuit_elements/operators/OpPrep.m
@@ -1,17 +1,20 @@
-function [OpN] = OpPrep(Op, qubit, dN)
+function [OpN] = OpPrep(Op, qubit, NoQ)
     %OPPREP Prepare an arbitrary operator to act on a density matrix, Example:
     %take S_x operator acting on the 1st qubit in a 3 qubit density matrix
-    % Op - the operator; qubit - #th cubit that Op acts on; dN - density matrix
-    % of dN qubits
+    % INPUT --------------------------------------------
+    % Op - [Matrix], the operator;
+    % qubit - [integer], #th cubit that Op acts on; 
+    % NoQ - [Integer], Number of qubits in the system
+    % INPUT --------------------------------------------
     
     if qubit == 1
         OpN = Op;
-        for i = 2:dN
+        for i = 2:NoQ
             OpN = kron(OpN, eye(2,2));
         end
     else
         OpN = eye(2,2);
-        for i = 2:dN
+        for i = 2:NoQ
             if i == qubit
                 OpN = kron(OpN, Op);
             else
