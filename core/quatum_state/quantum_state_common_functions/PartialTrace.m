ed7285e0d88ef7c5568fa602e0c3f2f77a09b416
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-15 17:24:52 +0200

    Update PartialTrace.m

--- core/quatum_state/quantum_state_common_functions/PartialTrace.m
+++ core/quatum_state/quantum_state_common_functions/PartialTrace.m
@@ -4,11 +4,11 @@ function [rho_Q] = PartialTrace(RhoV, q)
 % RhoV - density vector
 % q - the cubit that we want to trace out.
 
-NRhoV  = length(RhoV); %length of Rho vector
-NoQ   = round(log(sqrt(NRhoV))/log(2));  %Number of qubits in the system
+NRhoV   = length(RhoV);                     %length of Rho vector
+NoQ     = round(log(sqrt(NRhoV))/log(2));   %Number of qubits in the system
 
 if q > NoQ || q < 0
-    warning('Uh oh, no cubit there homie')
+    warning('There is no qubit there, q must be [1 N]')
 end
 
 %alpha       = 0:1:2^(2 * NoQ)-1;
@@ -21,21 +21,22 @@ index = zeros(2*NoQ, 1);
 
 for i = 1:2
     for j = 1:2
-        rho_Q(2*(i - 1) + j) = 0;
-        index(q) = i-1;
-        index(NoQ + q) = j-1;
-        for m = 0:(2^(NoQ-1) - 1)
+        rho_Q(2 * (i - 1) + j)  = 0;
+
+        index(q)        = i - 1;
+        index(NoQ + q)  = j - 1;
+
+        for m = 0:(2^(NoQ - 1) - 1)
             index_temp = flip(de2bi(m, NoQ - 1));
-            alphaBiL = [index_temp(1:q-1) (i-1) index_temp(q:end)];
-            alphaBiR = [index_temp(1:q-1) (j-1) index_temp(q:end)];
+
+            alphaBiL = [index_temp(1:q - 1) (i - 1) index_temp(q:end)];
+            alphaBiR = [index_temp(1:q - 1) (j - 1) index_temp(q:end)];
+
             alpha = bi2de(flip([alphaBiL alphaBiR])) + 1;
-            rho_Q(2*(i - 1) + j) = rho_Q(2 * (i - 1) + j) + RhoV(alpha);
+
+            rho_Q(2 * (i - 1) + j) = rho_Q(2 * (i - 1) + j) + RhoV(alpha);
         end
     end
 end
-
-
-
-
 end
 
