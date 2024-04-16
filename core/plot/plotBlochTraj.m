6f16e901f4760aa060ae198c3ec05c0facea454f
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-16 11:34:50 +0200

    Update plotBlochTraj.m

--- core/plot/plotBlochTraj.m
+++ core/plot/plotBlochTraj.m
@@ -1,4 +1,7 @@
 function [x,y,z] = plotBlochTraj(rhot, time, N)
+% This function will get a time series of a density matrix and the time steps, then it plots the
+% bloch vectors for this time-evolution:
+
     for t = 1:length(time)
         rho_temp = reshape(rhot(t, :, :), [2^N 2^N]);
         BlochVector = dm2bv(rho_temp);
