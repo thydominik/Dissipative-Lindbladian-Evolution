d0de0f0efbf2995bff9c93da72e367717172e40e
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-17 09:36:31 +0200

    Update OneQubitOp.m

--- core/circuit_elements/operators/OneQubitOp.m
+++ core/circuit_elements/operators/OneQubitOp.m
@@ -1,4 +1,4 @@
-function [U, LU] = OneQubitOp(G, q, rho)
+function [U, LU] = OneQubitOp(GateInput, qubit, rho)
 % NQUBITOP: Takes a one qubit gate and constructs the gate that acts
 % on the whole |rho) with that such that: G -> ...I x I x G x I x I... and
 % gives back the operator that acts on the Liouville space
@@ -6,33 +6,33 @@ function [U, LU] = OneQubitOp(G, q, rho)
 % NOTE: this is not an efficient way of acting with any gate!
 
 % INPUT --------------------------------------------
-% G - [Matrix], The quantum gate
+% GateInput - [Matrix], The quantum gate
 % rho - [Vector], density vector
-% q - [Integer], # of the first qubit that we want to act on
+% qubit - [Integer], # of the first qubit that we want to act on
 % INPUT --------------------------------------------
 
 Length_Rho_vec  = length(rho);                          %length of Rho vector
 NoQ             = round(log((Length_Rho_vec))/log(2));  %Number of qubits in the system
 
 % Building up the operator on Hilbert space:
-Length_G    = length(G);
+Length_G    = length(GateInput);
 NoQG        = round(log(Length_G)/log(2));  % how many qubits are involved in the gate action
 
 % populating the hilbert space gate with identity operators
-if q == 1 && (NoQG < NoQ)
-    U   = sparse(G);
+if qubit == 1 && (NoQG < NoQ)
+    U   = sparse(GateInput);
     U   = kron(U, speye(NoQ - 1));
     LU  = kron(U, conj(U));
 
-elseif q == NoQ && (NoQG == 1)
-    U   = sparse(G);
+elseif qubit == NoQ && (NoQG == 1)
+    U   = sparse(GateInput);
     U   = kron(speye(NoQ - 1), U);
     LU  = kron(U, conj(U));
 
-elseif (q + NoQG - 1) <= NoQ
-    U   = sparse(G);
-    U   = kron(speye(q - 1), U);
-    U   = kron(U, speye(NoQ - q));
+elseif (qubit + NoQG - 1) <= NoQ
+    U   = sparse(GateInput);
+    U   = kron(speye(qubit - 1), U);
+    U   = kron(U, speye(NoQ - qubit));
     LU  = kron(U, conj(U));
 
 else
