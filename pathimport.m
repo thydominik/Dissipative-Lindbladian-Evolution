66bf24793b1ea898df9081d27a933f7958e03ba2
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-19 12:33:55 +0200

    Update pathimport.m

--- pathimport.m
+++ pathimport.m
@@ -1,43 +1,82 @@
 function pathimport
 %PATHIMPORT This function includes all the necessary functions paths. This only includes the core folder and it's subfolders
+if ispc
 
+    addpath("core\")
 
-addpath("core\")
+    % Adding crcuit elements to the path
+    addpath("core\circuit_elements\dissipator_gates\")
+    addpath("core\circuit_elements\measurement\")
+    addpath("core\circuit_elements\operators\")
+    addpath("core\circuit_elements\quantum_gates\")
 
-% Adding crcuit elements to the path
-addpath("core\circuit_elements\dissipator_gates\")
-addpath("core\circuit_elements\measurement\")
-addpath("core\circuit_elements\operators\")
-addpath("core\circuit_elements\quantum_gates\")
+    disp("Circuit Elements were added.")
 
-disp("Circuit Elements were added.")
+    % Adding common_functions to the path
+    addpath("core\common_functions\")
 
-% Adding common_functions to the path
-addpath("core\common_functions\")
+    disp("Core Common Function were added.")
 
-disp("Core Common Function were added.")
+    % Misc
+    % addpath("core\misc\")
 
-% Misc
-% addpath("core\misc\")
+    % disp("Core Misc were added.")
 
-% disp("Core Misc were added.")
+    % Adding plots to the path
+    addpath("core\plot\")
 
-% Adding plots to the path
-addpath("core\plot\")
+    disp("Core plotting elements were added.")
 
-disp("Core plotting elements were added.")
+    % Adding quantum_states to the path
+    addpath("core\quatum_state\quantum_state_methods\")
+    addpath("core\quatum_state\qubits\")
+    addpath("core\quatum_state\quantum_state_common_functions\")
 
-% Adding quantum_states to the path
-addpath("core\quatum_state\quantum_state_methods\")
-addpath("core\quatum_state\qubits\")
-addpath("core\quatum_state\quantum_state_common_functions\")
+    disp("Core Quantum State functions were added.")
 
-disp("Core Quantum State functions were added.")
+    % Adding tim_evolution to the path
+    addpath("core\time_evolution\")
 
-% Adding tim_evolution to the path
-addpath("core\time_evolution\")
+    disp("Core Time evolution were added.")
 
-disp("Core Time evolution were added.")
+elseif ismac || isunix
 
+    addpath("core/")
+
+    % Adding crcuit elements to the path
+    addpath("core/circuit_elements/dissipator_gates/")
+    addpath("core/circuit_elements/measurement/")
+    addpath("core/circuit_elements/operators/")
+    addpath("core/circuit_elements/quantum_gates/")
+
+    disp("Circuit Elements were added.")
+
+    % Adding common_functions to the path
+    addpath("core/common_functions/")
+
+    disp("Core Common Function were added.")
+
+    % Misc
+    % addpath("core\misc\")
+
+    % disp("Core Misc were added.")
+
+    % Adding plots to the path
+    addpath("core/plot/")
+
+    disp("Core plotting elements were added.")
+
+    % Adding quantum_states to the path
+    addpath("core/quatum_state/quantum_state_methods/")
+    addpath("core/quatum_state/qubits/")
+    addpath("core/quatum_state/quantum_state_common_functions/")
+
+    disp("Core Quantum State functions were added.")
+
+    % Adding tim_evolution to the path
+    addpath("core/time_evolution/")
+
+    disp("Core Time evolution were added.")
+    
 end
 
