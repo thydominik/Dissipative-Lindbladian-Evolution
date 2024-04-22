343143ba7ffa9baacd7080636b3a7da85a1b9284
Dominik Szombathy <84455817+thydominik@users.noreply.github.com>
2024-04-22 10:39:31 +0200

    Update SpinOp.m

--- core/circuit_elements/operators/SpinOp.m
+++ core/circuit_elements/operators/SpinOp.m
@@ -3,15 +3,17 @@ function [S] = SpinOp(handle)
 % command SpinOp(handle); The 'handle' can be 1 - x, 2 - y, 3 - z, 4 -
 % p, 5 - m
 
-if handle == 1 || handle == 'x'
-    S = Pauli('x');
-elseif handle == 2 || handle == 'y'
-    S = Pauli('y');
-elseif handle == 3 || handle == 'z'
-    S = Pauli('z');
-elseif handle == 4 || handle == 'p'
+hbar = 1;
+
+if handle == 1 || strmpi(handle, 'x')
+    S = hbar/2 * Pauli('x');
+elseif handle == 2 || strmpi(handle, 'y')
+    S = hbar/2 * Pauli('y');
+elseif handle == 3 || strmpi(handle, 'z')
+    S = hbar/2 * Pauli('z');
+elseif handle == 4 || strmpi(handle, 'p') || strmpi(handle, '+')
     S = [0 1; 0 0];
-elseif handle == 5 || handle == 'm'
+elseif handle == 5 || strmpi(handle, 'm') || strmpi(handle, '-')
     S = [0 0; 0 1];
 else
     warning('Input not appropriate, try {(1, 2, 3, 4, 5) or {x, y, z, p, m}}')
